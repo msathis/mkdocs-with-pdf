2833d55cc1e35e6aea027d0043eb7804a4ae8405
sathis <sathis62@gmail.com>
2021-11-28 13:58:00 +0000

    populate cover_title always

--- mkdocs_with_pdf/options.py
+++ mkdocs_with_pdf/options.py
@@ -66,9 +66,9 @@ class Options(object):
         # Cover
         self.cover = local_config['cover']
         self.back_cover = local_config['back_cover']
-        if self.cover or self.back_cover:
-            self._cover_title = local_config['cover_title'] \
+        self._cover_title = local_config['cover_title'] \
                 if local_config['cover_title'] else config['site_name']
+        if self.cover or self.back_cover:
             self._cover_subtitle = local_config['cover_subtitle']
             self._cover_logo = local_config['cover_logo']
 
